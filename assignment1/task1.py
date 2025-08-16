cd68b0c2b7a0d020bd9b6598d7ff4a8397b852f7
Codefortoday <154976760+Codefortoday@users.noreply.github.com>
2025-08-16 21:52:39 +0530

    Add files via upload
    two task added

---
+++ assignment1/task1.py
@@ -0,0 +1,8 @@
+a=float(input("Enter first number: "))
+b=float(input("enter second number: "))
+
+print("Addition: ",(a+b))
+print("Subtraction: ",(a-b))
+print("Multipilcation: ",(a*b))
+print("Division: ",(a/b))
+print("Modulas: ",(a%b))
